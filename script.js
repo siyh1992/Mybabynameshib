d2c0864838cf0cc9430fecf46cddf5f21c56dd0c
siyh1992 <simonholt_14@hotmail.com>
2025-07-11 11:01:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('My Baby Names Hub loaded');
+
+function filterTable() {
+    const input = document.getElementById("search");
+    const filter = input.value.toLowerCase();
+    const table = document.getElementById("namesTable");
+    const trs = table.getElementsByTagName("tr");
+
+    for (let i = 1; i < trs.length; i++) {
+        let tds = trs[i].getElementsByTagName("td");
+        let match = false;
+        for (let j = 0; j < tds.length; j++) {
+            if (tds[j].textContent.toLowerCase().indexOf(filter) > -1) {
+                match = true;
+                break;
+            }
+        }
+        trs[i].style.display = match ? "" : "none";
+    }
+}
